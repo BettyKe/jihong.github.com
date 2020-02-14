37aed380b05d1bcd99a50ee4372b4ea16c749c4c
bettyke <724018828@qq.com>
2020-02-15 00:01:43 +0800

    ke

--- src/pages/my/information.vue
+++ src/pages/my/information.vue
@@ -2,11 +2,35 @@
     <div class="container_box">
         <header-box bgc="#fff" title="个人资料"></header-box>
         <div class="info_box bg_FFF">
-            <div class="user_avatar dfa">
+            <div class="box user_avatar dfb">
                 <span class="fs28 c_33292B">头像</span>
-                <div></div>
+                <div class="df ais jct-end">
+                    <img class="avatar" src="../../image/f_ic_soi_2@2x.png" alt="">
+                    <img class="img32" src="../../image/f_ic_more@2x.png" alt="">
+                </div>
+            </div>
+            <div class="box dfb">
+                <span class="fs28 c_33292B">昵称</span>
+                <div class="df ais jct-end">
+                    <span>吉祥花木园艺店</span>
+                    <img class="img32 ml20" src="../../image/f_ic_more@2x.png" alt="">
+                </div>
+            </div>
+            <div class="box dfb">
+                <span class="fs28 c_33292B">绑定手机</span>
+                <div class="df ais jct-end">
+                    <span>136****5234</span>
+                    <img class="img32 ml20" src="../../image/f_ic_more@2x.png" alt="">
+                </div>
+            </div>
+            <div class="box dfb">
+                <span class="fs28 c_33292B">修改密码</span>
+                <div class="df ais jct-end">
+                    <img class="img32 ml20" src="../../image/f_ic_more@2x.png" alt="">
+                </div>
             </div>
         </div>
+        <div class="btn_red">保存</div>
     </div>
 </template>
 <script>
@@ -15,14 +39,7 @@ export default {
     components: {
         headerBox,
     },
-    data() {
-        return {
-            list: [1,2,3,4,5,8],
-            loading: false,
-        }
-    }
 }
-
 </script>
 <style lang="less" scoped>
 .container_box{
@@ -36,8 +53,20 @@ export default {
 }
 .info_box{
     margin: 20px 30px 64px;
+    border-radius: 20px;
+    background: #fff;
+    .box{
+        border-top: 1px solid #F0F0F0;
+        height: 90px;
+        padding: 0 30px;
+    }
     .user_avatar{
-        
+        border-top:none;
+        height: 166px;
+        .avatar{
+            width: 108px;
+            height: 108px;
+        }
     }
 }
 </style>
