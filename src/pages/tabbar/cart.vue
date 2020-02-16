2986bb8c25918471f678e1997fd3ca607459e4a6
huwenpiao <502282517@qq.com>
2020-02-16 16:57:38 +0800

    hu

--- src/pages/tabbar/cart.vue
+++ src/pages/tabbar/cart.vue
@@ -1,10 +1,22 @@
 <template>
-    <van-list class="container_box ">
+    <div class="container_box bg_F7F5F6">
         <div class="cart_top df jct-between cf">
             <span class="fs34 b">购物车</span>
             <span class="fs28">管理</span>
         </div>
-    </van-list>
+        <van-list class="item_list">
+             <div class="item bg_FFF">
+                 <div class="dfb item_title">
+                     <div class="select dfc"><img class="img40" src="../../image/e_ic_circle_d@2x.png" /></div>
+                     <div class="flex">
+                         <img class="img32" src="../../image/e_ic_provider@2x.png" />
+                        <span>供应商名称供应商</span>
+
+                     </div>
+                 </div>
+             </div>
+        </van-list>
+    </div>
 </template>
 <script></script>
 <style scoped lang="less">
@@ -17,5 +29,24 @@
     box-sizing: border-box;
     padding: 68px 30px 0;
     background:linear-gradient(-35deg,rgba(223,1,52,1),rgba(240,70,70,1));
+}
+.item_list{
+    position: absolute;
+    top: 160px;
+    left: 0;
+    width: 100%;
+    height: calc(100% - 258px);
+    z-index: 2;
+    box-sizing: border-box;
+    overflow-y: scroll;
+}
+.item{
+    margin: 0 30px;
+    border-radius: 20px;
+    .item_title{
+        .select{
+            width: 90px;
+        }
+    }
 }
 </style>
